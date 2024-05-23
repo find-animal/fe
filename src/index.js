30e6c04265dd21feef92c85fb5ca44bdfd6dfe42
Jaminggu <sjmim0220@gmail.com>
2024-05-23 16:58:48 +0900

    https://github.com/find-animal/fe/tree/jamin

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 >>>>>>> 95c829c (Initialize project using Create React App)
 // If you want to start measuring performance in your app, pass a function
