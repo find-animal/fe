8babcc5c09f1ed864c387a69c9d9ad123b481f59
Jaminggu <sjmim0220@gmail.com>
2024-05-23 16:53:16 +0900

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+<<<<<<< HEAD
+=======
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+>>>>>>> 95c829c (Initialize project using Create React App)
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +15,14 @@ root.render(
   </React.StrictMode>
 );
 
+<<<<<<< HEAD
+=======
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+>>>>>>> 95c829c (Initialize project using Create React App)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
