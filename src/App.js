f937abe0e161d2f61c20920fe9f8e1a86eb4b512
Jaminggu <sjmim0220@gmail.com>
2024-05-25 07:50:34 +0700

    feat: add routing to MyPage component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import styles from "./App.styles.module.css";
-import MyPageRoutes from "./routes/MyPageRoutes";
-import MyPage from "./pages/MyPage";
+import Routers from "./components/Routers";
 
 function App() {
   return (
     <div className={styles.App}>
-      <MyPage />
+      <Routers />
     </div>
   );
 }
---
+++ src/components/Routers.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import React from "react";
+import MyPage from "../pages/MyPage";
+import ChangePwd from "../pages/MyPage/ChangePwd/ChangePwd";
+import Setting from "../pages/MyPage/Setting/Setting";
+import Notice from "../pages/MyPage/Notice/Notice";
+import LogOut from "../pages/MyPage/LogOut/LogOut";
+import AccountDeletion from "../pages/MyPage/AccountDeletion/AccountDeletion";
+import TermOfService from "../pages/MyPage/TermsOfService/TermOfService";
+import Layout from "./Layout";
+
+export default function Routers() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/mypage" element={<MyPage />}></Route>
+        <Route path="/mypage/change-password" element={<ChangePwd />} />
+        <Route path="/mypage/setting" element={<Setting />} />
+        <Route path="/mypage/notice" element={<Notice />} />
+        <Route path="/mypage/logout" element={<LogOut />} />
+        <Route path="/mypage/term-of-service" element={<TermOfService />} />
+        <Route path="/mypage/account-deletion" element={<AccountDeletion />} />
+      </Route>
+    </Routes>
+  );
+}
