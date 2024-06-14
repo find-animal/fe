3277b8b105a714d23ad31f6025a4c6d92fe86dd5
Jaminggu <sjmim0220@gmail.com>
2024-06-14 12:07:33 +0900

    feat: 로그인 기능 구현

--- src/pages/LogIn/index.jsx
+++ src/pages/LogIn/index.jsx
@@ -1,24 +1,58 @@
-import styles from "./index.styles.module.css"
+import styles from "./index.styles.module.css";
 import Button from "../../components/Button";
-import React, {useState} from "react";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import InputBox from "../../components/InputBox";
+import axios from "axios";
 
 export default function LogIn() {
+  const navigate = useNavigate();
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
+  const [check, setCheck] = useState(true);
+
+  const handleLogIn = async () => {
+    try {
+      axios
+        .post("/api/v1/user/login", {
+          id,
+          password,
+        })
+        .then((res) => {
+          const token = res.data.jwt;
+          if (token) {
+            localStorage.setItem("token", token);
+            navigate("/");
+          }
+        })
+        .catch((e) => {
+          setCheck(false);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className={styles.container}>
       <p className={styles.title}>로그인</p>
-      <InputBox type={'text'} text={'아이디'} onInputChange={setId}/>
-      <InputBox type={'password'} text={'비밀번호'} onInputChange={setPassword}/>
-      <div style={{marginBottom: '50px'}}></div>
-      <Button text={'로그인'}/>
+      <InputBox type={"text"} text={"아이디"} onInputChange={setId} />
+      <InputBox
+        type={"password"}
+        text={"비밀번호"}
+        onInputChange={setPassword}
+      />
+      {!check && (
+        <p className={styles.warning}>아이디 및 비밀번호를 확인해주세요.</p>
+      )}
+      <div style={{ marginBottom: "50px" }}></div>
+      <Button text={"로그인"} onClick={handleLogIn} />
       <div className={styles.link_container}>
-        <p><Link to={'/signup'}>회원가입</Link></p>
+        <p>
+          <Link to={"/signup"}>회원가입</Link>
+        </p>
         <p>비밀번호 찾기</p>
       </div>
     </div>
-  )
+  );
 }
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,10 +1,10 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from "./index.styles.module.css";
 import Button from "../../components/Button";
 import GoBackIcon from "../../components/GoBackIcon";
 import InputBox from "../../components/InputBox";
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
   const navigate = useNavigate();
@@ -19,7 +19,9 @@ export default function SignUp() {
     if (id.length < 4) {
       setError("아이디는 최소 4글자 이상이어야 합니다.");
       return;
-    } else if (!/(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\W)(?=\S+$).{8,16}/.test(password)) {
+    } else if (
+      !/(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\W)(?=\S+$).{8,16}/.test(password)
+    ) {
       setError("비밀번호는 8~16자 영문 대소문자, 숫자, 특수문자를 사용하세요.");
       return;
     } else if (password !== confirmPassword) {
@@ -28,34 +30,48 @@ export default function SignUp() {
     }
 
     try {
-      axios.post('/api/v1/user/signup', {
-        id,
-        password,
-      }).then(() => {
-        alert('회원가입 성공');
-        navigate('/login');
-      }).catch((err) => {
-        console.log(err);
-      })
+      axios
+        .post("/api/v1/user/signup", {
+          id,
+          password,
+        })
+        .then(() => {
+          alert("회원가입 성공");
+          navigate("/login");
+        })
+        .catch((err) => {
+          if (err.response && err.response.status === 400) {
+            setError("이미 사용 중인 아이디입니다.");
+          } else {
+            console.log(err);
+          }
+        });
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <GoBackIcon/>
+        <GoBackIcon />
         <p>회원가입</p>
       </div>
       <form onSubmit={handleSignUp}>
-        <InputBox type={'text'} text={'아이디'} onInputChange={setId}/>
-        <InputBox type={'password'} text={'비밀번호'} onInputChange={setPassword}/>
-        <InputBox type={'password'} text={'비밀번호 확인'} onInputChange={setConfirmPassword}/>
+        <InputBox type={"text"} text={"아이디"} onInputChange={setId} />
+        <InputBox
+          type={"password"}
+          text={"비밀번호"}
+          onInputChange={setPassword}
+        />
+        <InputBox
+          type={"password"}
+          text={"비밀번호 확인"}
+          onInputChange={setConfirmPassword}
+        />
         <p className={styles.warning}>{error}</p>
-        <Button text={'회원가입'} type="submit"/>
+        <Button text={"회원가입"} type="submit" />
       </form>
     </div>
-  )
-
+  );
 }
