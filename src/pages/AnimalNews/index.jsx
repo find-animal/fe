5549e163ecc05ca9a5908865f2a4995843729b86
Jaminggu <sjmim0220@gmail.com>
2024-07-02 11:44:03 +0900

    feat: '동물 소식' 페이지 뉴스 무한스크롤 구현

--- src/pages/AnimalNews/index.jsx
+++ src/pages/AnimalNews/index.jsx
@@ -3,16 +3,19 @@ import styles from "./index.styles.module.css";
 import axios from "axios";
 import AnimalNewsList from "./_components/AnimalNewsList";
 import Header from "../../components/Header";
+import {useInView} from "react-intersection-observer";
 
 export default function AnimalNews() {
   const [page, setPage] = useState(1);
   const [animalNewsList, setAnimalNewsList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [ref, inView] = useInView();
 
   const fetchAnimalNews = async () => {
     try {
       const res = await axios.get(`/api/v1/news/animal/${page}`);
       setAnimalNewsList([...animalNewsList, ...res.data.items]);
+      setPage(prev => prev + 1);
       setIsLoading(false);
     } catch (err) {
       console.log(err);
@@ -22,7 +25,7 @@ export default function AnimalNews() {
 
   useEffect(() => {
     fetchAnimalNews();
-  }, []);
+  }, [inView]);
 
   return (
     <div className={styles.container}>
@@ -32,7 +35,7 @@ export default function AnimalNews() {
           {isLoading && <p>Loading...</p>}
           {animalNewsList.map((list) => {
             return (
-              <div>
+              <div ref={ref}>
                 <AnimalNewsList list={list}/>
               </div>
             );
