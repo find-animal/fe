c69d322e87c463756034806d3c741e762eadf061
Jaminggu <sjmim0220@gmail.com>
2024-07-03 21:28:50 +0900

    feat: '동물 소식'페이지에 뉴스 이미지 추가

--- src/pages/AnimalNews/_components/AnimalNewsList.jsx
+++ src/pages/AnimalNews/_components/AnimalNewsList.jsx
@@ -3,13 +3,14 @@ import styles from "./AnimalNewsList.styles.module.css";
 import Divider from "../../../components/Divider";
 
 export default function AnimalNewsList({ list }) {
-  const { title, link, description, pubDate } = list;
+  const { title, image, link, description, pubDate } = list;
 
   const titleText = title.replace(/<b>/g, "").replace(/<\/b>/g, "").replace(/&quot;/g, '"');
   const descriptionText = description.replace(/<b>/g, "").replace(/<\/b>/g, "").replace(/&quot;/g, '"');
 
   return (
     <div className={styles.container} onClick={() => window.open(link)}>
+      {image && <div className={styles.image_container}><img src={image} alt="image"/></div>}
       <div className={styles.contents_container}>
         <p>{titleText}</p>
         <p>{descriptionText}</p>
