ef1af0b90e350f0d0fa80fdc69fe9b76ec319462
Jaminggu <sjmim0220@gmail.com>
2024-07-04 16:54:56 +0900

    feat: '회원 정보' 페이지에 아이디, 이메일 정보 추가

--- src/pages/MyPage/AccInform/AccInform.jsx
+++ src/pages/MyPage/AccInform/AccInform.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import styles from './AccInform.styles.module.css'
+import BackIconHeader from "../../../components/BackIconHeader";
+import axios from "axios";
 
 export default function AccInform() {
+  const id = localStorage.getItem('id');
+  const [email, setEmail] = useState('');
+
+  const getEmail = async () => {
+    try {
+      const res = await axios.get("/api/v1/user/email", {params: {id}});
+      setEmail(res.data.email);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(()=> {
+    getEmail();
+  }, [])
   return (
     <div>
-      <p>회원 정보</p>
+      <BackIconHeader text={'회원 정보'}/>
+      <div className={styles.contents}>
+        <h3>아이디</h3>
+        <p>{id}</p>
+        <h3>이메일</h3>
+        <p>{email}</p>
+      </div>
     </div>
   )
 }
