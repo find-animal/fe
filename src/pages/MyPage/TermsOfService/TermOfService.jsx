808b87e26d2886b6dbe73e535c948525c3b00388
Jaminggu <sjmim0220@gmail.com>
2024-07-10 23:49:19 +0900

    design: '이용약관' 페이지 헤더 디자인 변경

--- src/pages/MyPage/TermsOfService/TermOfService.jsx
+++ src/pages/MyPage/TermsOfService/TermOfService.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 import styles from "./TermOfService.styles.module.css";
-import GoBackIcon from "../../../components/GoBackIcon";
-import Index from "../../../components/BackIconHeader";
+import BackIconHeader from "../../../components/BackIconHeader";
 
 export default function TermOfService() {
-    return (
-        <div className={styles.container}>
-            <Index text={'이용약관'}/>
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius laborum recusandae maxime earum dolore
-                    eligendi culpa! Sapiente fugiat nihil ab cumque commodi. Dolores officiis est quibusdam expedita
-                    iste asperiores explicabo.
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate, illum eveniet cum, quam animi
-                    delectus minus architecto voluptatum illo consequatur id fugiat debitis in obcaecati perspiciatis
-                    deserunt, mollitia porro quas.
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam fuga autem rerum nemo quis,
-                    laboriosam sapiente molestias voluptas at aliquid vero id impedit? Quas, at saepe minus alias vel
-                    beatae?
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <BackIconHeader text={"이용약관"} />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius laborum
+        recusandae maxime earum dolore eligendi culpa! Sapiente fugiat nihil ab
+        cumque commodi. Dolores officiis est quibusdam expedita iste asperiores
+        explicabo. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Voluptate, illum eveniet cum, quam animi delectus minus architecto
+        voluptatum illo consequatur id fugiat debitis in obcaecati perspiciatis
+        deserunt, mollitia porro quas. Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Quam fuga autem rerum nemo quis, laboriosam sapiente
+        molestias voluptas at aliquid vero id impedit? Quas, at saepe minus
+        alias vel beatae?
+      </p>
+    </div>
+  );
 }
